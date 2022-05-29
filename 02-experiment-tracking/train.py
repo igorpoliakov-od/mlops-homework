82e5b9098b76b6fcf3e73a83fd999506c4379136
Igor Poliakov <igor.poliakov@onedome.com>
2022-05-29 22:19:59 +0300

    week 2

---
+++ 02-experiment-tracking/train.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+
+mlflow.set_tracking_uri("http://10.0.25.75:5000/")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+def run(data_path):
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    
+    # with mlflow.start_run():
+
+    mlflow.set_tag("developer", "igor")
+
+    mlflow.log_param("train-data-path", "train.pkl")
+    mlflow.log_param("valid-data-path", "valid.pkl")
+
+    mlflow.sklearn.autolog()
+
+    rf = RandomForestRegressor(max_depth=10, random_state=0)
+    rf.fit(X_train, y_train)
+    y_pred = rf.predict(X_valid)
+
+    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="data/",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
